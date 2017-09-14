f3dcc5037e1aeeaee262c1f8923b4ac83120fe2a
Bhuvanesh <blazingbhuvan@gmail.com>
2017-09-14 11:39:55 +0530

    Create SecureAuth.js

---
+++ SecureAuth.js
@@ -0,0 +1,8 @@
+(function(){
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://github.com/bhuvaneshs/Samples/blob/master/getip.js";
+    var x = document.getElementsByTagName("script")[0];
+    x.parentNode.insertBefore(s,x);
+})();
